5dcccff7c28bc99c37da46d2849e911d85f77e19
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-10-15 14:17:50 +0200

    api calls to fetch courses data to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import CourseDetails from './components/CourseDetails';
 
 function App() {
-  return <div className='App'>hello world</div>;
+  const [courses, setCourses] = useState([]);
+  const [course, setCourse] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          'https://private-e05942-courses22.apiary-mock.com/courses'
+        );
+        setCourses([...response.data]);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(course);
+
+  return (
+    <div className='App'>
+      <header>
+        <h1>Careerfoundry</h1>
+        <h3>Our courses: </h3>
+        <div>
+          {courses.map((course) => (
+            <button key={course.slug} onClick={() => setCourse(course)}>
+              {course.title}
+            </button>
+          ))}
+        </div>
+      </header>
+      <main>
+        {course && (
+          <div>
+            <CourseDetails {...course} />
+          </div>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CourseDetails.js
@@ -0,0 +1,37 @@
+import './CourseDetails.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function CourseDetails({ slug, title, url, next_start, next_start_formatted }) {
+  const [details, setDetails] = useState({});
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `https://private-e05942-courses22.apiary-mock.com/courses/${slug}`
+        );
+        setDetails(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, [slug]);
+
+  const { description, prices, start_dates } = details;
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <p>{next_start_formatted}</p>
+
+      {location.is_eu ? <p>{prices[1].amount}€</p> : <p>${prices[0].amount}</p>}
+    </div>
+  );
+}
+
+export default CourseDetails;
